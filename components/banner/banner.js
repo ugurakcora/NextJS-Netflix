200be788848dfeaa130ed63530a2e7c2bd0e2170
ugurakcora <uakcora97@gmail.com>
2023-10-05 17:29:21 +0300

    updated banner

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,4 +1,4 @@
-import style from "./banner.module.css";
+import styles from "./banner.module.css";
 
 const Banner = (props) => {
   const { title, subTitle, imgUrl } = props;
@@ -6,12 +6,25 @@ const Banner = (props) => {
     console.log("play");
   };
   return (
-    <div>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
+    <div className={styles.container}>
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+          <h3 className={styles.title}>{title}</h3>
+          <h3 className={styles.subTitle}>{subTitle}</h3>
+
+          <div className={styles.playBtnWrapper}>
+            <button className={styles.btnWithIcon} onClick={handleOnPlay}>
+              <span className={styles.playText}>Play</span>
+            </button>
+          </div>
+        </div>
+      </div>
       <div
-        className={style.img}
+        className={styles.bannerImg}
         style={{
           backgroundImage: `url(${imgUrl})`,
         }}
